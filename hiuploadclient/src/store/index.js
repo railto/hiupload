5089f825d68fb28b6c0ecab02b40a254a5285a98
Mark Railton <mark@markrailton.com>
2020-12-29 19:43:31 +0000

    adds list of files to client

--- hiuploadclient/src/store/index.js
+++ hiuploadclient/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex'
 import auth from './auth'
+import files from './files'
 
 export default createStore({
   state: {
@@ -10,5 +11,6 @@ export default createStore({
   },
   modules: {
     auth,
+    files,
   }
 })
---
+++ hiuploadclient/src/views/Upload.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="mb-8">
+      Uploader
+    </div>
+    <div>
+      <h2 class="font-medium pb-3 border-b-2 border-gray-100 text-gray-800">Your files</h2>
+
+      <template v-if="files.length">
+        <app-file v-for="file in files" :key="file.uuid" :file="file" />
+      </template>
+      <p v-else class="mt-3 text-sm text-gray-800">There are no files here right now</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapActions, mapGetters} from 'vuex'
+import AppFile from "@/components/AppFile";
+
+export default {
+  components: {
+    AppFile,
+  },
+
+  computed: {
+    ...mapGetters({
+      files: 'files/files',
+    }),
+  },
+
+  methods: {
+    ...mapActions({
+      getFiles: 'files/getFiles',
+    }),
+  },
+
+  mounted() {
+    this.getFiles();
+  }
+}
+</script>
